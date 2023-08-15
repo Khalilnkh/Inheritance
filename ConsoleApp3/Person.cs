cc20931aabc8b08bbc6b0c556d66f281e067b26f
Khalilnkh <khalilnkh@code.edu.az>
2023-08-15 16:36:56 +0400

    Updated

--- ConsoleApp3/Person.cs
+++ ConsoleApp3/Person.cs
@@ -6,14 +6,16 @@ namespace ConsoleApp3
 {
     public class Person
     {
-        public string name;
-        public string surname;
-        public int age;
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int  Age { get; set; }
+
+
         public Person(string name, string surname, int age)
         {
-            this.name = name;
-            this.surname = surname;
-            this.age = age;
+            Name = name;
+          Surname = surname;
+            Age = age;
         }
     }
 }
